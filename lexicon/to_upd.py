43266bb29329e9507a4b21df02450f73d3c7487c
Михаил Борисов <borisov.uk@gmail.com>
2024-02-16 13:35:48 +0300

    add okavango

--- lexicon/to_upd.py
+++
@@ -1,9 +0,0 @@
-"""
-    ModelD(brand='Geely', model='Okavango', engine='2.0T', power='200', torque='325',
-           acceleration='9.6', consumption='7.7', transmission='роботизированная DCT7',
-           size='4860 x 1910 x 1770', mass='1780', fuel='60', clearance='184',
-           drive='передний', susp='независимая McPherson\nнезависимая многорычажная',
-           equip='Luxury Flagship',
-           price='3 449 990 - 3 599 990', page='https://www.geely-motors.com/model/geely-okavango/'),
-
-"""
